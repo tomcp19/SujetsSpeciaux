b075479f8d17d9245839a66a399c2d75ceef748f
Nicolas Bourré <nbourrereg@hotmail.com>
2020-09-08 13:26:52 -0400

    Add project files.

---
+++ FileScanner/ViewModels/MainViewModel.cs
@@ -0,0 +1,84 @@
+﻿using FileScanner.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace FileScanner.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string selectedFolder;
+        private ObservableCollection<string> folderItems = new ObservableCollection<string>();
+         
+        public DelegateCommand<string> OpenFolderCommand { get; private set; }
+        public DelegateCommand<string> ScanFolderCommand { get; private set; }
+
+        public ObservableCollection<string> FolderItems { 
+            get => folderItems;
+            set 
+            { 
+                folderItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedFolder
+        {
+            get => selectedFolder;
+            set
+            {
+                selectedFolder = value;
+                OnPropertyChanged();
+                ScanFolderCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            OpenFolderCommand = new DelegateCommand<string>(OpenFolder);
+            ScanFolderCommand = new DelegateCommand<string>(ScanFolder, CanExecuteScanFolder);
+        }
+
+        private bool CanExecuteScanFolder(string obj)
+        {
+            return !string.IsNullOrEmpty(SelectedFolder);
+        }
+
+        private void OpenFolder(string obj)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    SelectedFolder = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void ScanFolder(string dir)
+        {
+            FolderItems = new ObservableCollection<string>(GetDirFiles(dir));
+         
+            
+            foreach (var item in Directory.EnumerateFiles(dir, "*"))
+            {
+                FolderItems.Add(item);
+            }
+        }
+
+        IEnumerable<string> GetDirFiles(string dir)
+        {
+            foreach (var item in Directory.EnumerateDirectories(dir, "*"))
+            {
+                yield return item;
+            }
+        }
+
+
+    }
+}
