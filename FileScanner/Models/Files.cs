f155f684b01d8f60436e1735f12b43bc872da95b
Tom <tomcp19@gmail.com>
2020-09-13 16:37:41 -0400

    TP1 - FileScanner

---
+++ FileScanner/Models/Files.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FileScanner.Models
+{
+    public class Files : INotifyPropertyChanged
+    {
+        private string filePath;
+        private string fileIconPath;
+        private string folderIconPath;
+        public string FilePath { 
+            get => filePath;
+            set
+            {
+                filePath = value;
+                OnPropertyChanged();
+            } 
+        }
+        public string FileIconPath
+        {
+            get => fileIconPath;
+            set
+            {
+                fileIconPath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FolderIconPath
+        {
+            get => folderIconPath;
+            set
+            {
+                folderIconPath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Files(string _filePath, string _folderIconPath, string _fileIconPath)
+        {
+            filePath = _filePath;
+            fileIconPath = _fileIconPath;
+            folderIconPath = _folderIconPath;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- FileScanner/ViewModels/MainViewModel.cs
+++ FileScanner/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using FileScanner.Commands;
+using FileScanner.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,13 +9,17 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
 
+
+//référence du datagrid / datatemplate pour le xaml https://www.codeproject.com/Questions/204314/How-do-I-add-an-image-and-data-to-a-WPF-Data-Grid + lab de l'hiver passé
+
 namespace FileScanner.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
         private string selectedFolder;
         private ObservableCollection<string> folderItems = new ObservableCollection<string>();
-         
+        private ObservableCollection<Files> itemsList = new ObservableCollection<Files>();
+
         public DelegateCommand<string> OpenFolderCommand { get; private set; }
         public DelegateCommand<string> ScanFolderCommand { get; private set; }
 
@@ -23,6 +28,16 @@ namespace FileScanner.ViewModels
             set 
             { 
                 folderItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Files> ItemsList
+        {
+            get => itemsList;
+            set
+            {
+                itemsList = value;
                 OnPropertyChanged();
             }
         }
@@ -41,7 +56,7 @@ namespace FileScanner.ViewModels
         public MainViewModel()
         {
             OpenFolderCommand = new DelegateCommand<string>(OpenFolder);
-            ScanFolderCommand = new DelegateCommand<string>(ScanFolder, CanExecuteScanFolder);
+            ScanFolderCommand = new DelegateCommand<string>(ScanFolderAsync, CanExecuteScanFolder);
         }
 
         private bool CanExecuteScanFolder(string obj)
@@ -60,25 +75,67 @@ namespace FileScanner.ViewModels
             }
         }
 
-        private void ScanFolder(string dir)
+        //références
+        //https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-enumerate-directories-and-files
+        //https://docs.microsoft.com/fr-fr/dotnet/csharp/programming-guide/concepts/async/
+        //https://docs.microsoft.com/en-us/dotnet/standard/exceptions/how-to-use-the-try-catch-block-to-catch-exceptions
+
+        private async void ScanFolderAsync(string dir) 
         {
-            FolderItems = new ObservableCollection<string>(GetDirFiles(dir));
-         
-            
-            foreach (var item in Directory.EnumerateFiles(dir, "*"))
+            ItemsList.Clear(); //vider la liste entre 2 scans
+            await Task.Run(() =>
             {
-                FolderItems.Add(item);
-            }
+                FolderItems = new ObservableCollection<string>(GetDirFiles(dir));
+                try 
+                {//try
+
+
+                    foreach (var folder in Directory.EnumerateDirectories(dir, "*", SearchOption.AllDirectories))
+                    {//fe1
+                        var newFolder = new Files(folder, "/images/folder.png", ""); //https://icon-icons.com/
+                        //j'ai voulu m'amuser avec l'idendation si folder ou file, j'ai donc mis un icon par colonne... voir xaml)
+
+
+                        //https://stackoverflow.com/questions/2137769/where-do-i-get-a-thread-safe-collectionview?fbclid=IwAR1NAWrwFGoLUq9wkZCgdUL-2qSKdu56-gHTbk1nSwwSe2xN05slv-as2-g
+                        System.Windows.Application.Current.Dispatcher.BeginInvoke(
+                        System.Windows.Threading.DispatcherPriority.Normal,
+                        (Action)delegate ()
+                        {
+                            ItemsList.Add(newFolder);
+                        });
+                        /*
+                         https://docs.microsoft.com/en-us/dotnet/api/system.io.directory.enumeratefiles?view=netcore-3.1
+                         En placant le foeach avec le folder en cours, ca nous permet de lister les fichiers sous chaque dossiers qui les contient, au lieu de tous
+                         les dossiers et ensuite tous les fichiers
+                         
+                         */
+                        foreach (var files in Directory.EnumerateFiles(folder, "*"))
+                        {//fe2
+                            var newFiles = new Files(files, "", "/images/file.png"); //https://icon-icons.com/
+                             System.Windows.Application.Current.Dispatcher.BeginInvoke(
+                             System.Windows.Threading.DispatcherPriority.Normal,
+                             (Action)delegate ()
+                             {
+                                 ItemsList.Add(newFiles);
+                             });
+                        }//fe2
+                    }//fe1
+
+                }//try
+                catch (System.UnauthorizedAccessException)
+                {
+
+                    System.Windows.MessageBox.Show("Vous n'avez pas les accès nécessaires pour scanner tout cet emplacement" );
+                }
+            });
         }
 
-        IEnumerable<string> GetDirFiles(string dir)
+            IEnumerable<string> GetDirFiles(string dir)//envoi file et dir
         {
-            foreach (var item in Directory.EnumerateDirectories(dir, "*"))
+            foreach (var folder in Directory.EnumerateDirectories(dir, "*"/*, SearchOption.AllDirectories*/))
             {
-                yield return item;
+                 yield return folder;            
             }
         }
-
-
     }
 }
